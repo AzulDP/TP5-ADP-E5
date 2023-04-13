99c11035f44e455793db70185a72b74e57354461
Azul De Pol <azul.depol@gmail.com>
2023-04-13 18:33:20 +0100

    empeze la logica del cronometro

--- js/TP5-ejercicio5.js
+++ js/TP5-ejercicio5.js
@@ -1,6 +1,12 @@
 /* Cronómetro - Dificultad:  🟢🟡
 
 5- Realizar una web con un cronómetro, que tenga las opciones de iniciar, reset (volver el cronómetro a 0) y pausar.
-
-
 */
+
+let contador =1;
+function cronometro(){
+    document.write(`<p>Prueba de set interval N=${contador}</p>`)
+    contador++;
+}
+
+const idInterval = setInterval(cronometro, 1000);
