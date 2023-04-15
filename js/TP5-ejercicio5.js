7ab323f98a4d564fcba01806986e49b3c9c42d10
Azul De Pol <azul.depol@gmail.com>
2023-04-15 18:09:30 +0100

    actualize el navbar y el estilo del h1

--- js/TP5-ejercicio5.js
+++ js/TP5-ejercicio5.js
@@ -2,7 +2,14 @@
 
 5- Realizar una web con un cronómetro, que tenga las opciones de iniciar, reset (volver el cronómetro a 0) y pausar.
 */
+// let botonStart = document.querySelector("")
 
+function start (){
+    console.log("Desde Iniciar");
+    let btnIniciar = document.getElementById("Iniciar");
+    console.log(btnIniciar.className);
+
+}
 let contador =1;
 function cronometro(){
     document.write(`<p>Prueba de set interval N=${contador}</p>`)
