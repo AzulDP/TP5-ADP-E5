23a83224863c6ae4d03f79b41f4c39c7ebce59b6
Azul De Pol <azul.depol@gmail.com>
2023-04-18 17:23:57 +0100

    agregue logica del cronometro

--- js/TP5-ejercicio5.js
+++ js/TP5-ejercicio5.js
@@ -4,62 +4,72 @@
 */
 // let botonStart = document.querySelector("")
 let cronometro = document.getElementById("cronometro");
-let btnIniciar = document.getElementById("iniciar");
-let btnPausar = document.getElementById("pausar");
-let btnParar = document.getElementById("parar");
-let s = 0;
-let m = 0;
-let h = 0;
+let pausa = true;
+let sec = 0;
+let min = 0;
+let hrs = 0;
 let t;
 
-function marker (){
+function mostrar (){
    sec++;
    if (sec <= 60){
-    sec= 0;
-    min++;
+    if (sec < 10){
+        sec = "0" + sec;
+    } else{
+        sec= 0;
+        min++;
+    }
     if(min <=60){
-        min =0;
-        hrs++;
+        if (min < 10){
+            min = "0" + min;
+        } else{
+            min =0;
+            hrs++;
+        }
+    }
+    if (hrs < 99){
+        if (hrs < 10){
+        hrs = "0" + hrs;
+    } else {
+        hrs = 0;
     }
 }
 }
+document.getElementById("cronometro").innerHTML = `${hrs} : ${min} : ${sec}`;
+}
 
-function add(){
-    marker();
-    cronometro.innerHTML = (hrs > 9 ? hrs : "0" + hrs) 
-+ ":" + (min > 9 ? min : "0" + min)
-   + ":" + (sec > 9 ? sec : "0" + sec);
-   timer();
-}
-function timer (){
-    t = setTimeout(add,1000);
-}
+// function add(){
+//     marker();
+//    document.getElementById("cronometro").innerHTML = (hrs > 9 ? hrs : "0" + hrs) 
+// + ":" + (minmin > 9 ?min : "0" +min) + ":" + (sec > 9 ? sec : "0" + sec);
+//    timer();
+// }
+// function timer (){
+//     t = setTimeout(add,1000);
+// }
 
 
 
 function start (){
     console.log("Desde Iniciar");
     console.log(btnIniciar.className);
+    // const idInterval = setInterval(cronometro, 1000);
     start.onclick = timer;
+    document.getElementById("cronometro").innerHTML = `${hrs}:${min}:${sec}`;
 }
 
 function stop(){ 
     stop.onclick = function (){
     clearTimeout(t);
+    document.getElementById("cronometro").innerHTML = `${hrs}:${min}:${sec}`;
 }
 }
 
 function pause(){
     pausar.onclick = function() {
-        h1.innerHTML = "00:00:00";
-        seconds = 0; minutes = 0; hours = 0;
+      document.getElementById("cronometro").innerHTML = `${hrs}:${min}:${sec}`;
+        sec = 0; min = 0; hrs = 0;
     }
 }
-// }
-// let contador =1;
-// function cronometro(){
-//     document.write(`<p>Prueba de set interval N=${contador}</p>`)
-//     contador++;
-// }
 
-// const idInterval = setInterval(cronometro, 1000);
+
